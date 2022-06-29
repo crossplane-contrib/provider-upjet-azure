92f612682155fefb16843e84ebeff91d63696da3
ezgidemirel <ezgidemirel91@gmail.com>
2022-06-29 16:57:57 +0300

    Use AssumeRole struct and update kubebuilder validations for auth fields
    Signed-off-by: ezgidemirel <ezgidemirel91@gmail.com>

--- provider-azure/apis/v1beta1/types.go
+++ provider-azure/apis/v1beta1/types.go
@@ -39,7 +39,7 @@ type ProviderConfigSpec struct {
 // ProviderCredentials required to authenticate.
 type ProviderCredentials struct {
 	// Source of the provider credentials.
-	// +kubebuilder:validation:Enum=None;Secret;InjectedIdentity;Environment;Filesystem
+	// +kubebuilder:validation:Enum=None;Secret;ManagedSystemAssignedIdentity;ManagedUserAssignedIdentity
 	Source xpv1.CredentialsSource `json:"source"`
 
 	xpv1.CommonCredentialSelectors `json:",inline"`
