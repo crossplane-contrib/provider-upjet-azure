2ee18af22ae00fd46897797c6c9208d82246d24b
nullable.eth <2248325+nullable-eth@users.noreply.github.com>
2022-08-11 09:09:28 -0400

    updates

--- provider-azure/apis/generate.go
+++ provider-azure/apis/generate.go
@@ -41,6 +41,9 @@ limitations under the License.
 // Generate crossplane-runtime methodsets (resource.Claim, etc)
 //go:generate go run -tags generate github.com/crossplane/crossplane-tools/cmd/angryjet generate-methodsets --header-file=../hack/boilerplate.go.txt ./...
 
+// Run UpDoc generator
+//go:generate go run ../../updoc/cmd/main.go generate --docs-dir=../../docs
+
 package apis
 
 import (
@@ -49,4 +52,8 @@ import (
 	_ "github.com/crossplane/crossplane-tools/cmd/angryjet" //nolint:typecheck
 
 	_ "github.com/upbound/upjet/cmd/scraper"
+
+	_ "github.com/alecthomas/kong"
+
+	_ "github.com/upbound/official-providers/updoc/internal"
 )
