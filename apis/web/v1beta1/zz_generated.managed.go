41281fa67907c135e67fd5721d9b4ff37d43e708
kdibrov <kdibrov@gmail.com>
2023-02-14 13:40:08 +0100

    Adding web (1) resource

--- apis/web/v1beta1/zz_generated.managed.go
+++ apis/web/v1beta1/zz_generated.managed.go
@@ -7,6 +7,72 @@ package v1beta1
 
 import xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 
+// GetCondition of this AppActiveSlot.
+func (mg *AppActiveSlot) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this AppActiveSlot.
+func (mg *AppActiveSlot) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetProviderConfigReference of this AppActiveSlot.
+func (mg *AppActiveSlot) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+/*
+GetProviderReference of this AppActiveSlot.
+Deprecated: Use GetProviderConfigReference.
+*/
+func (mg *AppActiveSlot) GetProviderReference() *xpv1.Reference {
+	return mg.Spec.ProviderReference
+}
+
+// GetPublishConnectionDetailsTo of this AppActiveSlot.
+func (mg *AppActiveSlot) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
+// GetWriteConnectionSecretToReference of this AppActiveSlot.
+func (mg *AppActiveSlot) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this AppActiveSlot.
+func (mg *AppActiveSlot) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this AppActiveSlot.
+func (mg *AppActiveSlot) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetProviderConfigReference of this AppActiveSlot.
+func (mg *AppActiveSlot) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+/*
+SetProviderReference of this AppActiveSlot.
+Deprecated: Use SetProviderConfigReference.
+*/
+func (mg *AppActiveSlot) SetProviderReference(r *xpv1.Reference) {
+	mg.Spec.ProviderReference = r
+}
+
+// SetPublishConnectionDetailsTo of this AppActiveSlot.
+func (mg *AppActiveSlot) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
+}
+
+// SetWriteConnectionSecretToReference of this AppActiveSlot.
+func (mg *AppActiveSlot) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this AppServicePlan.
 func (mg *AppServicePlan) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)
