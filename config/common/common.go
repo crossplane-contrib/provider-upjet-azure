398046d007c5fdcbecf7c5162ff011343f6c31c6
svscheg <schegolevatych@gmail.com>
2023-03-13 22:27:15 +0200

    Deleting the explicit sensitive configuration for the azurerm_kubernetes_cluster

--- config/common/common.go
+++ config/common/common.go
@@ -25,6 +25,8 @@ import (
 	tjconfig "github.com/upbound/upjet/pkg/config"
 
 	"github.com/upbound/provider-azure/apis/rconfig"
+
+	"github.com/crossplane/crossplane-runtime/pkg/fieldpath"
 )
 
 const (
@@ -148,4 +150,11 @@ func addReference(references tjconfig.References, referenceKind, referenceName, 
 			Extractor: rconfig.ExtractResourceIDFuncPath,
 		}
 	}
+}
+
+// GetField returns the value of field as a string in a map[string]interface{},
+//
+//	fails properly otherwise.
+func GetField(from map[string]interface{}, path string) (string, error) {
+	return fieldpath.Pave(from).GetString(path)
 }
