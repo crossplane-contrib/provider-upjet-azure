a7dc7035a87276b3770ef7fc758ad61978c11758
Jan Mikołajczak <extern.mikolajczak_jan@allianz.de>
2023-08-09 07:43:48 +0200

    [load]: lint and remove not needed files

--- config/externalname.go
+++ config/externalname.go
@@ -1704,7 +1704,7 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	// /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.StreamAnalytics/streamingJobs/job1/inputs/input1
 	"azurerm_stream_analytics_reference_input_mssql": config.TemplatedStringAsIdentifier("name", "/subscriptions/{{ .setup.configuration.subscription_id }}/resourceGroups/{{ .parameters.resource_group_name }}/providers/Microsoft.StreamAnalytics/streamingJobs/{{ .parameters.stream_analytics_job_name }}/inputs/{{ .external_name }}"),
 
-    // load
+	// load
 	//
 	// An existing Load Test can be imported into Terraform using the resource id
 	// /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.LoadTestService/loadTests/{loadTestName}
