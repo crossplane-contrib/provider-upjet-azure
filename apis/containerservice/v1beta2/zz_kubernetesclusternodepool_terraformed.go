16ac8dd27a39f1f91d4cb72d68cbaf61ce3eac97
Mikel Landa <mikel.landa@pepperservicing.es>
2024-11-22 19:58:37 +0100

    fix node_count in initProvider for KubernetesClusterNodePool
    Signed-off-by: Mikel Landa <mikel.landa@pepperservicing.es>

--- apis/containerservice/v1beta2/zz_kubernetesclusternodepool_terraformed.go
+++ apis/containerservice/v1beta2/zz_kubernetesclusternodepool_terraformed.go
@@ -118,6 +118,11 @@ func (tr *KubernetesClusterNodePool) LateInitialize(attrs []byte) (bool, error) 
 		return false, errors.Wrap(err, "failed to unmarshal Terraform state parameters for late-initialization")
 	}
 	opts := []resource.GenericLateInitializerOption{resource.WithZeroValueJSONOmitEmptyFilter(resource.CNameWildcard)}
+	initParams, err := tr.GetInitParameters()
+	if err != nil {
+		return false, errors.Wrapf(err, "cannot get init parameters for resource '%q'", tr.GetName())
+	}
+	opts = append(opts, resource.WithConditionalFilter("NodeCount", initParams))
 
 	li := resource.NewGenericLateInitializer(opts...)
 	return li.LateInitialize(&tr.Spec.ForProvider, params)
