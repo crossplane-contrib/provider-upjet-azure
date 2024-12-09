ffccf9970663a7bd92e0b9cb1cf4ca9131493669
Mikel Landa <mikel.landa@pepperservicing.es>
2024-12-09 16:58:03 +0100

    add azurerm_storage_container_immutability_policy
    Signed-off-by: Mikel Landa <mikel.landa@pepperservicing.es>

--- apis/storage/v1beta1/zz_generated.managedlist.go
+++ apis/storage/v1beta1/zz_generated.managedlist.go
@@ -52,6 +52,15 @@ func (l *BlobList) GetItems() []resource.Managed {
 	return items
 }
 
+// GetItems of this ContainerImmutabilityPolicyList.
+func (l *ContainerImmutabilityPolicyList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this ContainerList.
 func (l *ContainerList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
