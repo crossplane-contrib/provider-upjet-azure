472066f1f3d595c0925b02b8bbdfb1b5be143222
Yury Tsarev <yury@upbound.io>
2025-05-20 11:03:44 +0200

    Add results of `make generate` for mssqlmanagedinstancetransparentdataencryption
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- apis/sql/v1beta1/zz_generated.managed.go
+++ apis/sql/v1beta1/zz_generated.managed.go
@@ -727,6 +727,66 @@ func (mg *MSSQLManagedInstanceFailoverGroup) SetWriteConnectionSecretToReference
 	mg.Spec.WriteConnectionSecretToReference = r
 }
 
+// GetCondition of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetManagementPolicies of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetManagementPolicies() xpv1.ManagementPolicies {
+	return mg.Spec.ManagementPolicies
+}
+
+// GetProviderConfigReference of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+// GetPublishConnectionDetailsTo of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
+// GetWriteConnectionSecretToReference of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetManagementPolicies of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetManagementPolicies(r xpv1.ManagementPolicies) {
+	mg.Spec.ManagementPolicies = r
+}
+
+// SetProviderConfigReference of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+// SetPublishConnectionDetailsTo of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
+}
+
+// SetWriteConnectionSecretToReference of this MSSQLManagedInstanceTransparentDataEncryption.
+func (mg *MSSQLManagedInstanceTransparentDataEncryption) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this MSSQLManagedInstanceVulnerabilityAssessment.
 func (mg *MSSQLManagedInstanceVulnerabilityAssessment) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)
