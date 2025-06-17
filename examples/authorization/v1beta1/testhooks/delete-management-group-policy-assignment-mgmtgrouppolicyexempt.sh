07241ab7a6dd2213439f7360603f476ab5749abe
daftping <21245083+daftping@users.noreply.github.com>
2025-06-16 19:21:51 -0700

    fix: avoid race condition in tests where management group assignment is deleted before the management group itself, causing a 403 error on assignment deletion
    Signed-off-by: daftping <21245083+daftping@users.noreply.github.com>

---
+++ examples/authorization/v1beta1/testhooks/delete-management-group-policy-assignment-mgmtgrouppolicyexempt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+# SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+# Remove ManagementGroupPolicyAssignment first before removing ManagementGroup
+# If deletion executed in parallel the ManagementGroupPolicyAssignment
+# will stuck with
+# ...unexpected status 403 (403 Forbidden) with error: AuthorizationFailed: The client '$uuid' with object id '$uuid' does not have authorization to perform action 'Microsoft.Authorization/policyAssignments/read' over scope '/providers/Microsoft.Management/managementGroups/<name>/providers/Microsoft.Authorization/policyAssignments/<name>' or the scope is invalid. If access was recently granted, please refresh your credentials.
+${KUBECTL} delete managementgrouppolicyassignment.authorization.azure.upbound.io mgmtgrouppolicyexempt
