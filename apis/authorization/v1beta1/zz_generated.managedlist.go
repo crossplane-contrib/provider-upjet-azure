9c247fa8e198bad4cf6bec3673400f90dafea825
daftping <21245083+daftping@users.noreply.github.com>
2025-06-20 21:38:22 -0700

    feat: add azurerm_policy_set_definition
    Signed-off-by: daftping <21245083+daftping@users.noreply.github.com>

--- apis/authorization/v1beta1/zz_generated.managedlist.go
+++ apis/authorization/v1beta1/zz_generated.managedlist.go
@@ -61,6 +61,15 @@ func (l *PolicyDefinitionList) GetItems() []resource.Managed {
 	return items
 }
 
+// GetItems of this PolicySetDefinitionList.
+func (l *PolicySetDefinitionList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this ResourceGroupPolicyAssignmentList.
 func (l *ResourceGroupPolicyAssignmentList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
